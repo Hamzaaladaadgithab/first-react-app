dcfcfe1ee75f6f9cb675f736ef4a80aa896b1a61
Hamzaaladaadgithab <aladaada5105390@gmail.com>
2025-11-02 13:48:58 +0300

    react te baslama noktası  component oluşturma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MyFirstComponent from "./MyFirstComponent";
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to refresh.
         </p>
+
+        <h1>Tarmeez</h1>
+
+        <MyFirstComponent />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,11 @@
+export default function MyFirstComponent() {
+  return (
+    <div>
+      <h1>Hello Hamza</h1>
+
+      <h2>Hi.</h2>
+
+      <button>Click Me...</button>
+    </div>
+  );
+}
